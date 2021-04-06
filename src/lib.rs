19b6f3bae71d80179483bc42c5f9ea274eeae2f5
Ross MacArthur <ross@macarthur.io>
2021-04-06 22:10:27 +0200

    Add crate level documentation

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,25 @@
+//! This crate provides extended methods to types that implement
+//! [`Read`][std::io::Read] and [`Write`][std::io::Write].
+//!
+//! # Examples
+//!
+//! ```
+//! use std::io;
+//! use io_ext::*;
+//!
+//! fn main() -> io::Result<()> {
+//!     let mut data = io::Cursor::new(vec![0x37, 0x13, 0x13, 0x37]);
+//!
+//!     let x: u16 = data.read_le()?;
+//!     assert_eq!(x, 0x1337);
+//!
+//!     let y: u16 = data.read_be()?;
+//!     assert_eq!(y, 0x1337);
+//!
+//!     Ok(())
+//! }
+//! ```
+
 mod read;
 mod write;
 
